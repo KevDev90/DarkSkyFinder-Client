f97295c5370c761de70d24e861fbc7b9aa45b5bc
Kevin Johnson <kevinj902302@gmail.com>
2021-03-29 15:51:57 -0600

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import ApiContext from '../ApiContext'
+import config from '../config'
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+      folders: [],
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  handleAddCard = () => {
+
+  }
+
+  handleDeleteCard = () => {
+
+  }
+
+
+
+}
+
