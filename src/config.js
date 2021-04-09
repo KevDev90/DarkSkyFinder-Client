e732d2ad21c1a07b06bddf23871913e5b6bf508a
Kevin Johnson <kevinj902302@gmail.com>
2021-04-08 22:21:41 -0600

    updates config var

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_ENDPOINT: 'https://darksky-api-kevdev90.herokuapp.com',
-    // API_ENDPOINT: 'http://localhost:3000',
+    // API_ENDPOINT: 'http://localhost:8000',
     API_KEY: process.env.REACT_APP_API_KEY,
   }
